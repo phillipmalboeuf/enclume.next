ea5a37a25c287f7798764bbbd92abe78e3a7ea47
Phillip Malboeuf <phil@phils.computer>
2023-11-09 12:06:44 -0500

    optional gallery

--- app/projets/[id]/page.tsx
+++ app/projets/[id]/page.tsx
@@ -66,7 +66,7 @@ export default async function Projet({
         <div
           data-parallax="2"
           className='grid grid--thick_guttered grid--spaced_around grid--middle'>
-          {project.fields.gallery.map((photo: any, index: number)=> <OnScroll key={photo.fields.file.url} className={`col col--${project.fields.galleryGridSizes && project.fields.galleryGridSizes[index]}of12 col--tablet_portrait--12of12`}>
+          {project.fields.gallery?.map((photo: any, index: number)=> <OnScroll key={photo.fields.file.url} className={`col col--${project.fields.galleryGridSizes && project.fields.galleryGridSizes[index]}of12 col--tablet_portrait--12of12`}>
             <figure>
               <Picture src={photo.fields.file.url} />
               {photo.fields.description && <figcaption><small>{photo.fields.description}</small></figcaption>}
